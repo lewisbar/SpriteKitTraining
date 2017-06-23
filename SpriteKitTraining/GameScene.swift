8d74e522330be6b8e6c972d2d4f341de49aa8a59
Lennart Wisbar <lewisbar.git@gmail.com>
2017-06-23 07:37:08 +0200

    The floor is moving, the ball rolling and can be grabbed and dropped.

--- SpriteKitTraining/GameScene.swift
+++ SpriteKitTraining/GameScene.swift
@@ -12,22 +12,28 @@ import GameplayKit
 class GameScene: SKScene {
     
     let ball = SKSpriteNode(imageNamed: "StarBall")
-    let floor = SKSpriteNode(imageNamed: "Floor")
+    let floor1 = SKSpriteNode(imageNamed: "Floor")
+    let floor2 = SKSpriteNode(imageNamed: "Floor")
     var initialBallPosition: CGPoint!
     var initialBallSize: CGSize!
     
     override func didMove(to view: SKView) {
-        
-        initialBallPosition = CGPoint(x: ball.size.width, y: floor.size.height + ball.size.height / 2)
+        initialBallPosition = CGPoint(x: ball.size.width, y: floor1.size.height + ball.size.height / 2)
         initialBallSize = ball.size
         
         backgroundColor = .lightGray
         
-        floor.anchorPoint = .zero
-        floor.position = .zero
-        floor.zPosition = 1
-        floor.size.width = self.size.width
-        self.addChild(floor)
+        floor1.anchorPoint = .zero
+        floor1.position = .zero
+        floor1.zPosition = 1
+        floor1.size.width = self.size.width
+        self.addChild(floor1)
+        
+        floor2.anchorPoint = .zero
+        floor2.position = CGPoint(x: floor1.size.width, y: 0)
+        floor2.zPosition = 1
+        floor2.size.width = self.size.width
+        self.addChild(floor2)
         
         ball.position = initialBallPosition
         ball.zPosition = 2
@@ -36,11 +42,21 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         ball.zRotation -= .pi * 5 / 180
+        
+        floor1.position.x -= 4
+        floor2.position.x -= 4
+        
+        if floor1.position.x < -floor1.size.width {
+            floor1.position.x = floor2.position.x + floor2.size.width
+        } else if floor2.position.x < -floor2.size.width {
+            floor2.position.x = floor1.position.x + floor1.size.width
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
-            ball.position = touch.location(in: floor)
+            ball.position.x = touch.location(in: view).x
+            ball.position.y = -touch.location(in: view).y + (view?.bounds.height)!
             ball.size.width *= 2
             ball.size.height *= 2
         }
@@ -48,13 +64,14 @@ class GameScene: SKScene {
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
-            ball.position = touch.location(in: floor)
+            ball.position.x = touch.location(in: view).x
+            ball.position.y = -touch.location(in: view).y + (view?.bounds.height)!
         }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
-            let fingerPosition = touch.location(in: floor)
+            let fingerPosition = touch.location(in: view)
             self.ball.position = CGPoint(x: fingerPosition.x, y: self.initialBallPosition.y)
             self.ball.size = self.initialBallSize
         }
